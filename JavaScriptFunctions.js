625cb9c21286f9dd0ea7ed4130a8f8340698e9de
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2021-12-28 00:16:03 +0530

    JavaScript: Arrays Recap, HTML Quiz, Functions Overview

---
+++ JavaScriptFunctions.js
@@ -0,0 +1,23 @@
+/*
+FUNCTIONS: (METHOD)
+1. FUNCTION DEFINITION
+2. FUNCTION CALL (INVOKE)
+*/
+
+// 1. FUNCTION DEFINITION
+// function fun1(){
+//   let a = 2;
+//   let b = 6;
+//   console.log(a+b)
+// }
+
+// 2. FUNCTION CALL
+console.log(1)
+console.log(2)
+fun1()
+console.log(3)
+console.log(4)
+fun1()
+console.log(5)
+console.log(6)
+
