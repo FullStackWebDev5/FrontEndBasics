8cc61e853db7536716a4c9554d79489ea1877cb0
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2021-12-29 00:43:57 +0530

    JavaScript: Functions, Default Paramaters, Anonymous Functions, Function Expressions, DSA Problems

--- JavaScriptFunctions.js
+++ JavaScriptFunctions.js
@@ -5,11 +5,11 @@ FUNCTIONS: (METHOD)
 */
 
 // 1. FUNCTION DEFINITION
-// function fun1(){
-//   let a = 2;
-//   let b = 6;
-//   console.log(a+b)
-// }
+function fun1(){
+  let a = 2;
+  let b = 6;
+  console.log(a+b)
+}
 
 // 2. FUNCTION CALL
 console.log(1)
@@ -21,3 +21,8 @@ fun1()
 console.log(5)
 console.log(6)
 
+// function fun1(){
+//   let a = 2;
+//   let b = 6;
+//   console.log(a+b)
+// }
